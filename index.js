ccdf6fe076f7a20d331f8dda56b88ba14a67edb1
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2021-01-31 16:07:55 -0500

    updating index

--- index.js
+++ index.js
@@ -32,15 +32,16 @@ app.get("/players", (req, res) => {
     res.send(importData)
 })
 
-app.listen(port, () => {
-    console.log('Example app is listening on http://localhost:${port}')
-})
+app.listen(port)
+// app.listen(port, () => {
+//     console.log('Example app is listening on http://localhost:${port}')
+// })
 
 //step 4: make a new repository exd-api
 //step 5: create .gitignore
 //setp : create Procfile file - no extention
 //step : clone repository:  1) cd exd-api 2) cd exd-api
 //step 6: push to Git Hub repository 
-//1) git init 2) git add . 3) git commit -m "first commit"  4) 'git push orgin main' 
+//1) git init 2) git add . 3) git commit -m "first commit"  4) 'git push origin main' 
 //step 7: push to Heroku server - login 
 //step : at the terminal type 1) 'heroku create exd-api' 2) 'git push heroku main' 3) click deploy link

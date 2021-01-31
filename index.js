5d5f4cff5a0f87bc9d43f2dcf89a941bd4b16b81
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2021-01-30 22:10:57 -0500

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let port = process.env.port || 3000
 //step 2
 app.get("/", (req, res) => {
     res.send("Hello World")
-})
+}) 
 
 //step 3
 app.get("/players", (req, res) => { 
@@ -22,3 +22,8 @@ app.get("/players", (req, res) => {
 app.listen(port, () => {
     console.log('Example app is listening on http://localhost:${port}')
 })
+
+//step 4:  download and install heroku close folder and reopen.  login
+//setp 5: create Procfile
+//step 6:  in terminal heroku create my-app and start pushing changes using git commit 
+//step 7: 1) git add .  2) git commit -m "first commit"  3) git push heroku master
